c7d432ea1ecd951437e454870c679290e1fe36f2
jayanthchandra <jayanthchandra14@gmail.com>
2015-10-30 21:44:50 +0530

    First Test Commit

---
+++ Test/test.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+int main(){
+
+	Mat im = imread("test.jpg");
+	if (im.empty()){
+
+		cout << "Cannot load image!" << endl;
+		return -1;
+
+	}
+
+	imshow("Image", im);
+	waitKey(0);
+
+}
